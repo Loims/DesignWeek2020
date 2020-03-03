b5ecb33695261159ee35b35e3d87cb71b4f791b2
Loims <34040415+Loims@users.noreply.github.com>
2020-03-03 11:11:34 -0500

    Add object pooler and base projectile
    - Add object pooler to ensure lag-free gameplay
    - Add base projectile to be used by enemies. All projectiles will inherit from this

---
+++ DesignWeekProject/Assets/Scripts/AISpawner.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AISpawner : MonoBehaviour
+{
+    public static AISpawner instance = null;
+
+    private void Awake()
+    {
+        ////Singleton initialization////
+        DontDestroyOnLoad(this.gameObject);
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+        /////////////////////////////////
+
+
+    }
+}
