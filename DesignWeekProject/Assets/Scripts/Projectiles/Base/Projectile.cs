0ff438cecab848195149626c4d852520d8edf938
Loims <34040415+Loims@users.noreply.github.com>
2020-03-03 12:51:08 -0500

    Add bullet base colors and random color assignment
    - Bullets now have colors they can be assigned
    - If not assigned a color, bullets will randomly choose one
    - Update folder structure

--- DesignWeekProject/Assets/Scripts/Projectiles/Base/Projectile.cs
+++ DesignWeekProject/Assets/Scripts/Projectiles/Base/Projectile.cs
@@ -4,7 +4,21 @@ using UnityEngine;
 
 public class Projectile : MonoBehaviour
 {
+    public enum Color
+    {
+        NULL,
+        RED,
+        BLUE,
+        PURPLE
+    }
+
     #region Variables
+    [Header("Color Materials (Test Only)")]
+    [SerializeField] private Material redMat;
+    [SerializeField] private Material blueMat;
+    [SerializeField] private Material purpleMat;
+    
+    [Space]
     [Header("Object Pooler")]
     [SerializeField] private ObjectPooler pooler;
 
@@ -13,32 +27,88 @@ public class Projectile : MonoBehaviour
 
     [SerializeField] private Rigidbody2D rb2d;
     [SerializeField] private float lifetime = 5f;
+    [SerializeField] private Color projectileColor;
     #endregion
 
     private void Awake()
     {
         pooler = ObjectPooler.instance;
         rb2d = GetComponent<Rigidbody2D>();
+
+        redMat = Resources.Load<Material>("Red");
+        blueMat = Resources.Load<Material>("Blue");
+        purpleMat = Resources.Load<Material>("Purple");
     }
 
     private void OnEnable()
     {
         StartCoroutine(BulletLifeTime(lifetime));
+        if(projectileColor == Color.NULL)
+        {
+            Color newColor = (Color)Random.Range(1, 3);
+            Debug.Log(newColor);
+            AssignColor(newColor);
+        }
     }
 
     private void OnDisable()
     {
+        projectileColor = Color.NULL;
         StopAllCoroutines();
     }
 
-    public virtual void InitializeBullet(Vector3 bulletVelocity)
+    /*<summary>
+     * Method to give bullet velocity. Should be called
+     * when bullet is instantiated anfd by the object instantiating it.
+     * If no velocity is provided, bullet will have velocity of 0.
+     * Useful if bullet has different functionality.
+     * </summary>
+     */
+    public virtual void InitializeBulletVelocity(Vector3 bulletVelocity)
     {
         rb2d.velocity = bulletVelocity;
     }
 
-    public virtual IEnumerator BulletLifeTime(float waitTime)
+    /*<summary>
+     * Method used to assign color. Method should be called
+     * when bullet is instantiated and by the object instantiating 
+     * it. If no color is assigned by the parent object, the projectile
+     * will be randomly assigned a color by OnEnable
+     * </summary>
+     */
+    public void AssignColor(Color color)
+    {
+        projectileColor = color;
+
+        Renderer renderer = GetComponent<Renderer>();
+        if (color == Color.RED)
+        {
+            renderer.material = redMat;
+        }
+        else if (color == Color.BLUE)
+        {
+            renderer.material = blueMat;
+        }
+        else if (color == Color.PURPLE)
+        {
+            renderer.material = purpleMat;
+        }
+    }
+
+    /*<summary>
+     * Lifetime coroutine. When the timer reaches 0,
+     * the projectile is returned to the object pool.
+     * To change lifetime, edit the lifetime variable
+     * </summary>
+     */
+    public IEnumerator BulletLifeTime(float waitTime)
     {
         yield return new WaitForSeconds(waitTime);
         pooler.ReturnToPool(this.gameObject);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        
     }
 }
