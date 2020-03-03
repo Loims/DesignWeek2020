4ce09c097647daad77c3b73fab4fa6e96c149ac0
durochal <57152028+durochal@users.noreply.github.com>
2020-03-03 13:30:05 -0500

    Credits created
    Scrolls down to credits and back again

---
+++ DesignWeekProject/Assets/Scripts2/CreditPressed.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CreditPressed : MonoBehaviour
+{
+    public static bool credits;
+    public static bool back;
+
+    void Start()
+    {
+        credits = false;
+        back = false;
+    }
+
+    public void CreditsClicked()
+    {
+        credits = true;
+        back = false;
+    }
+
+    public void BackClicked()
+    {
+        back = true;
+        credits = false;
+    }
+}
---
+++ DesignWeekProject/Assets/Scripts2/MoveToCredits.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveToCredits : MonoBehaviour
+{
+    public Vector3 cameraPos;
+    public Vector3 cameraMovedPos;
+    public Vector3 refVelocity = Vector3.zero;
+    public bool movedDown;
+    public bool movedUp;
+
+    void Start()
+    {
+        cameraPos = transform.position;
+        cameraMovedPos = new Vector3(0, -10, -10);
+        movedDown = false;
+        movedUp = false;
+    }
+
+    void LateUpdate()
+    {
+        if (CreditPressed.credits == true && movedDown == false)
+        {
+            MoveDown();
+        }
+
+        if (CreditPressed.back == true && movedUp == false)
+        {
+            MoveUp();
+        }
+    }
+
+    void MoveDown()
+    {
+        transform.position = Vector3.SmoothDamp(transform.position, cameraMovedPos, ref refVelocity, 0.75f);
+        if (transform.position.y < -9.99)
+        {
+            movedDown = true;
+            movedUp = false;
+        }
+    }
+
+    void MoveUp()
+    {
+        transform.position = Vector3.SmoothDamp(transform.position, cameraPos, ref refVelocity, 0.75f);
+        if (transform.position.y > -0.01 )
+        {
+            movedUp = true;
+            movedDown = false;
+        }
+    }
+}
