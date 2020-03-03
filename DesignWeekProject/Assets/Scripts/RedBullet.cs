43c507afe6d0c3f614fc25fc080efbd484f34b02
Loims <34040415+Loims@users.noreply.github.com>
2020-03-03 15:23:26 -0500

    Impliment modular bullets
    - Add modular bullets. Bullets change color based on the tag of the enemy
    - Import asset package

---
+++ DesignWeekProject/Assets/Scripts/RedBullet.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RedBullet : MonoBehaviour
+{
+    public string targetType="red";
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.GetComponent<RedEnemy>() != null)
+        {
+            Destroy(collision.gameObject);
+        }
+    }
+
+}
---
+++ DesignWeekProject/Assets/bossSpray.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bossSpray : MonoBehaviour
+{
+    public GameObject rotPoint;
+    public float time;
+    int direction = 1;
+    public float RotationSpeed;
+    bool circleSpray = false;
+    bool halCircleSpray = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (halCircleSpray)
+        {
+            time += Time.deltaTime;
+
+            transform.RotateAround(rotPoint.transform.position, new Vector3(0, 0, 1 * direction), RotationSpeed * Time.deltaTime);
+            if (time > 2.0f)
+            {
+                direction = -1 * direction;
+                time = 0.0f;
+            }
+        }
+        if (circleSpray)
+        {
+            transform.RotateAround(rotPoint.transform.position, new Vector3(0, 0, 1), RotationSpeed * Time.deltaTime);
+        }
+    }
+}
+
