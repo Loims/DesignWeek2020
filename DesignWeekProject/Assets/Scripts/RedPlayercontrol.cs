ae1072e4689f418b07ab85b89d1d3782b71f4504
Abhishektippi <abhishekreddycool@gmail.com>
2020-03-03 15:08:28 -0500

    minor Tweaks

--- DesignWeekProject/Assets/Scripts/RedPlayercontrol.cs
+++ DesignWeekProject/Assets/Scripts/RedPlayercontrol.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RebPlayercontrol : MonoBehaviour
+public class RedPlayercontrol : MonoBehaviour
 {
     public float planespeed = 10.0f;
     public GameObject bulletPrefab;
