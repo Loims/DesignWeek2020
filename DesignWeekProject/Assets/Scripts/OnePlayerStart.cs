600cc066052b48285b16e85d2482f8cdd77f4ec2
durochal <57152028+durochal@users.noreply.github.com>
2020-03-03 11:19:48 -0500

    Scene stuff
    Scene change and spawning players

---
+++ DesignWeekProject/Assets/Scripts/OnePlayerStart.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class OnePlayerStart : MonoBehaviour
+{
+    public static int playerCount;
+
+    public void StartOnePlayer()
+    {
+        SceneManager.LoadScene("Main_Stage");
+        playerCount = 1;
+    }
+}
